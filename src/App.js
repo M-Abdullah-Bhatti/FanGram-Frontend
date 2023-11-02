4d0f3a223cc246c91517ed6401d2169d36cc3747
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-11-02 05:39:42 -0700

    import issue resolved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import './App.css';
 // import FeaturedCelebrity from './components/FeaturedCelebrity';
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-
-        <Route path="/" element={<Home/>}></Route>
-      {/* <FeaturedCelebrity /> */}
+        <Route path="/" element={<Home />}></Route>
+        {/* <FeaturedCelebrity /> */}
       </Routes>
     </div>
   );
