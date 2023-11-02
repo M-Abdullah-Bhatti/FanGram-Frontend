c0645bb4e308ee2ca40797644e5bc6a6eb777329
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-11-02 06:27:58 -0700

    footer initiated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        {/* <Route path="/" element={<Home />}></Route> */}
         {/* <FeaturedCelebrity /> */}
       </Routes>
       <Footer />
