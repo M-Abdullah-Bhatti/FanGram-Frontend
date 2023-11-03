949b4c2575786d9675a5458bf29970487f325086
abdulwahid5605 <abdulwahid4888888@gmail.com>
2023-11-03 05:06:04 -0700

    Signup Login Second

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./components/Login/Login";
 import Footer from "./components/Shared/Footer";
 import Header from "./components/Shared/Header";
 import Home from "./pages/Home";
-import CelebrityPage from "./pages/CelebrityPage";
+// import CelebrityPage from "./pages/CelebrityPage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
