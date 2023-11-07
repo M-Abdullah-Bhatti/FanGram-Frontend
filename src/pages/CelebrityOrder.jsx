5bc131a4903f1cd0df2b300545e89a7ce67358e1
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-11-07 13:18:25 +0500

    Celebrity Order Page Done and dusted

--- src/pages/CelebrityOrder.jsx
+++ src/pages/CelebrityOrder.jsx
@@ -9,30 +9,6 @@ import StepsProgress from "../components/CelebrityOrder/StepsProgress";
 function CelebrityOrder() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const firstDivStyles = {
-    backgroundColor: currentIndex >= 0 ? "#D42978" : ''
-  }
-
-  const secondDivStyles = {
-    backgroundColor: currentIndex >= 1 ? "#D42978" : '',
-    borderWidth: currentIndex < 1 ? 1 : '0',
-    borderColor: currentIndex < 1 ? '#FFFFFF' : '',
-  }
-
-  const thirdDivStyles = {
-    backgroundColor: currentIndex > 1 ? "#D42978" : '',
-    borderWidth: currentIndex < 2 ? 1 : '0',
-    borderColor: currentIndex < 2 ? '#FFFFFF' : '',
-  }
-
-  const firstLineStyles = {
-    backgroundColor: currentIndex > 0 ? "#D42978" : '#FFFFFF',
-  }
-
-  const secondLineStyles = {
-    backgroundColor: currentIndex > 1 ? "#D42978" : '#FFFFFF',
-  }
-
   return (
     <div className="bg-[#000] py-12 flex flex-col items-center overflow-hidden">
       {/* Steps Progress */}
