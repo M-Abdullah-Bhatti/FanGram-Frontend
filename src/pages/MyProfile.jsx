de34aca9d84b3bf5cf13ecb1b27a057dee32ac91
abdulwahid5605 <abdulwahid4888888@gmail.com>
2023-11-07 04:54:02 -0800

    minor changes

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import MyProfileForm from "../components/MyProfilePage/MyProfileForm";
 import BottomLinks from "../components/MyProfilePage/BottomLinks";
 import TopLinks from "../components/MyProfilePage/TopLinks";
+import DiscoverMore from "../components/CelebrityPage/DiscoverMore";
 
 const MyProfile = () => {
   return (
     <div className="px-16 py-6 bg-black text-white">
       <TopLinks />
       <MyProfileForm />
-      <BottomLinks />
+      <DiscoverMore />
     </div>
   );
 };
