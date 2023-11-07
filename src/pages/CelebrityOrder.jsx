fe963ae3ca32dd9712bdd914b7ef15306e057e63
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-11-07 13:18:05 +0500

    Celebrity Order Page Done and dusted

--- src/pages/CelebrityOrder.jsx
+++ src/pages/CelebrityOrder.jsx
@@ -4,28 +4,39 @@ import HorizontalCurl from "../components/CelebrityOrder/HorizontalCurl";
 import OrderDetails from "../components/CelebrityOrder/OrderDetails";
 import Payment from "../components/CelebrityOrder/Payment";
 import Confirmation from "../components/CelebrityOrder/Confirmation";
+import StepsProgress from "../components/CelebrityOrder/StepsProgress";
 
 function CelebrityOrder() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const firstDivStyles = {
+    backgroundColor: currentIndex >= 0 ? "#D42978" : ''
+  }
+
+  const secondDivStyles = {
+    backgroundColor: currentIndex >= 1 ? "#D42978" : '',
+    borderWidth: currentIndex < 1 ? 1 : '0',
+    borderColor: currentIndex < 1 ? '#FFFFFF' : '',
+  }
+
+  const thirdDivStyles = {
+    backgroundColor: currentIndex > 1 ? "#D42978" : '',
+    borderWidth: currentIndex < 2 ? 1 : '0',
+    borderColor: currentIndex < 2 ? '#FFFFFF' : '',
+  }
+
+  const firstLineStyles = {
+    backgroundColor: currentIndex > 0 ? "#D42978" : '#FFFFFF',
+  }
+
+  const secondLineStyles = {
+    backgroundColor: currentIndex > 1 ? "#D42978" : '#FFFFFF',
+  }
+
   return (
     <div className="bg-[#000] py-12 flex flex-col items-center overflow-hidden">
       {/* Steps Progress */}
-
-      <div className="border-2 flex justify-start items-center">
-        {/* First Div */}
-        <div className="border-2 flex justify-start items-center">
-          <div className="bg-[#D42978] w-[40px] h-[40px] rounded-full"></div>
-          <div className="w-[300px] h-[3px] bg-[#D42978]"></div>
-        </div>
-        {/* Second Div */}
-        <div className="border-2 flex justify-start items-center">
-          <div className="bg-[#D42978] w-[40px] h-[40px] rounded-full"></div>
-          <div className="w-[300px] h-[3px] bg-[#D42978]"></div>
-        </div>
-        {/* Third Div */}
-        <div className="bg-[#D42978] w-[40px] h-[40px] rounded-full"></div>
-      </div>
+      <StepsProgress currentIndex={currentIndex} />
 
       <HorizontalCurl />
       <div className="px-20">
