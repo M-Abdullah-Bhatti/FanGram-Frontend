5f862eaf8998985872a5afa1f0341751a79f42ef
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-11-07 18:37:49 +0500

    Booking listing page done

--- src/components/MyProfilePage/BookingListingCard.jsx
+++ src/components/MyProfilePage/BookingListingCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import StepsProgress from '../CelebrityOrder/StepsProgress'
+import BookingStepsProgress from './BookingStepsProgress'
 
-function BookingListingCard() {
+function BookingListingCard({booking}) {
   return (
     <div className="flex gap-6 bg-[#202020] rounded-lg px-6 py-3">
         <div className="max-h-[250px] rounded-2xl overflow-hidden border border-[#ccc]"
@@ -12,18 +12,44 @@ function BookingListingCard() {
         }}
         >
             <img
-            src="/images/sunny.png"
+            src={booking.img}
             alt="celebrity"
             className="w-full h-[275px] object-fill"
             />
         </div>
-        <div>
-            <h1 className="font-extrabold text-4xl">Sunny Leone</h1>
-            <div>
-                <div></div>
+        <div className='w-full'>
+            <h1 className="font-extrabold text-4xl">{booking.name}</h1>
+            <div className="flex w-full py-4">
+                <div className="flex w-2/3 items-center font-semibold">
+                  <div className="w-1/2 border-r-2">
+                    <div className="flex justify-between mb-4 font-semibold">
+                      <span className="w-full text-[#7E7E7E]">Request For</span>
+                      <span className="w-full text-left">Recorder Video</span>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="w-full text-[#7E7E7E]">Booking Date</p>
+                      <p className='w-full text-left'>19 Oct, 2023</p>
+                    </div>
+                  </div>
+                  <div className="w-1/2">
+                    <div className="flex justify-between mb-4">
+                      <span className="w-full ml-12 text-[#7E7E7E]">Order Id</span>
+                      <span className="w-full">630114</span>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="w-full ml-12 text-[#7E7E7E]">Amount</p>
+                      <p className='w-full'>₹1,999 <sub className="text-[#7E7E7E] text-xs">(INC. OF GST)</sub></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-1/3 flex items-end flex-col gap-1 font-semibold">
+                  <p className="text-[#7E7E7E]">Booking For</p>
+                  <p>Saurav gannguly</p>
+                  <p className="text-[#D42978] underline">Need help?</p>
+                </div>
             </div>
             <div>
-                <StepsProgress currentIndex={2} />
+                <BookingStepsProgress currentIndex={2} />
             </div>
         </div>
     </div>
---
+++ src/components/MyProfilePage/BookingStepsProgress.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function BookingStepsProgress({currentIndex}) {
+
+    const firstDivStyles = {
+        backgroundColor: currentIndex >= 0 ? "#D42978" : ''
+    }
+    
+    const secondDivStyles = {
+        backgroundColor: currentIndex >= 1 ? "#D42978" : '',
+        borderWidth: currentIndex < 1 ? 1 : '0',
+        borderColor: currentIndex < 1 ? '#FFFFFF' : '',
+    }
+    
+    const thirdDivStyles = {
+        backgroundColor: currentIndex > 1 ? "#D42978" : '',
+        borderWidth: currentIndex < 2 ? 1 : '0',
+        borderColor: currentIndex < 2 ? '#FFFFFF' : '',
+    }
+    
+    const firstLineStyles = {
+        backgroundColor: currentIndex > 0 ? "#D42978" : '#FFFFFF',
+    }
+    
+    const secondLineStyles = {
+        backgroundColor: currentIndex > 1 ? "#D42978" : '#FFFFFF',
+    }
+
+  return (
+    <div className="flex justify-start items-center my-3">
+        {/* First Div */}
+        <div>
+          <div className="flex justify-start items-center mb-2">
+            <div className="bg-[#D42978] w-[40px] h-[40px] rounded-full" style={firstDivStyles}></div>
+            <div className="w-[250px] h-[2px]" style={firstLineStyles}></div>
+          </div>
+          <span style={{color: currentIndex >= 0 ? '#D42978' : '#FFFFFF', marginLeft: '-8px' }}>Details</span>
+        </div>
+        {/* Second Div */}
+        <div>
+          <div className="flex justify-start items-center mb-2">
+            <div className="w-[40px] h-[40px] rounded-full" style={secondDivStyles}></div>
+            <div className="w-[250px] h-[2px]" style={secondLineStyles}></div>
+          </div>
+          <span style={{color: currentIndex >= 1 ? '#D42978' : '#FFFFFF', marginLeft: '-10px' }}>Confirm</span>
+        </div>
+        {/* Third Div */}
+        <div>
+          <div className="flex justify-start items-center mb-2">
+            <div className="w-[40px] h-[40px] rounded-full" style={thirdDivStyles}></div>
+            <div className="w-[250px] h-[2px]" style={secondLineStyles}></div>
+          </div>
+          <span style={{color: currentIndex > 1 ? '#D42978' : '#FFFFFF', marginLeft: '-12px' }}>Payment</span>
+        </div>
+        {/* Fourth Div */}
+        <div>
+          <div className="flex justify-start items-center mb-2">
+            <div className="w-[40px] h-[40px] rounded-full" style={thirdDivStyles}></div>
+          </div>
+          <span style={{color: currentIndex > 1 ? '#D42978' : '#FFFFFF', marginLeft: '-12px' }}>Payment</span>
+        </div>
+    </div>
+  )
+}
+
+export default BookingStepsProgress
