8c75966b31fad4817d5514eb0afcbd5e225f3ef0
ShayanJamil519 <shayanjamil500@gmail.com>
2023-11-09 10:21:46 +0500

    Login Integration in Process

---
+++ src/services/auth-services.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import apiUrl from "../utils/baseURL";
+
+class AuthService {
+  /**
+   *User Signup
+   * @returns
+   */
+
+  async signUpUser(userData) {
+    console.log("Hello");
+    const res = await axios.post(`${apiUrl}/api/auth/signup`, userData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  }
+
+  /**
+   *User Login
+   * @returns
+   */
+
+  async LoginUser(userData) {
+    const res = await axios.post(`${apiUrl}/api/auth/login`, userData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  }
+
+  
+  
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new AuthService();
