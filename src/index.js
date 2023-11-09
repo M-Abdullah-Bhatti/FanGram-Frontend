c97b9e9e8957689507fe44f0fd8d41b089dd3be3
ShayanJamil519 <shayanjamil500@gmail.com>
2023-11-09 10:50:16 +0500

    Login Auth In Progress

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import { BrowserRouter } from "react-router-dom";
 import { StateProvider } from "./StateContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +20,9 @@ root.render(
     <ToastContainer position="top-right" autoClose={3000} />
     <BrowserRouter>
       <StateProvider>
-      <App />
+          <QueryClientProvider client={queryClient}>
+        <App />
+        </QueryClientProvider>
       </StateProvider>
     </BrowserRouter>
   </React.StrictMode>
