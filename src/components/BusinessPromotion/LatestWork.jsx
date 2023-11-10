319b87678865f2e1de6d61dd0a998dcd5c783770
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-11-10 11:46:34 +0500

    updates

--- src/components/BusinessPromotion/LatestWork.jsx
+++ src/components/BusinessPromotion/LatestWork.jsx
@@ -6,8 +6,8 @@ const work = [1, 2, 3, 4, 5, 6];
 function LatestWork() {
   return (
     <div className="mt-6">
-        <h2 className="font-semibold text-[45px]">0ur Latest Work</h2>
-        <div className="flex flex-wrap gap-4">
+        <h2 className="font-semibold text-xl md:text-2xl lg:text-[45px]">0ur Latest Work</h2>
+        <div className="flex flex-wrap gap-1 md:gap-4">
             {
                 work.map((index)=> (
                     <WorkCard key={index} />
