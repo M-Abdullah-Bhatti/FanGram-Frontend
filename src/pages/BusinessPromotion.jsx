feadb782b33a034390a7a96e7a75ccba90305d06
ShayanJamil519 <shayanjamil500@gmail.com>
2023-11-11 09:34:26 +0500

    Shoutout Section Added

--- src/pages/BusinessPromotion.jsx
+++ src/pages/BusinessPromotion.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import CTA from '../components/BusinessPromotion/CTA'
-import PromotionForm from '../components/BusinessPromotion/PromotionForm'
-import LatestWork from '../components/BusinessPromotion/LatestWork'
+import React from "react";
+import CTA from "../components/BusinessPromotion/CTA";
+import PromotionForm from "../components/BusinessPromotion/PromotionForm";
+import LatestWork from "../components/BusinessPromotion/LatestWork";
+import CelebrityShoutouts from "../components/BusinessPromotion/CelebrityShoutouts.jsx";
 
 function BusinessPromotion() {
   return (
     <div className="text-white">
-        <CTA />
-        <div className="w-full px-6 md:px-12 lg:px-24 bg-[#000]">
-            <PromotionForm />
-            <LatestWork />
-        </div>
+      <CTA />
+      <div className="w-full px-6 md:px-12 lg:px-24 bg-[#000]">
+        <PromotionForm />
+        <LatestWork />
+        <CelebrityShoutouts />
+      </div>
     </div>
-  )
+  );
 }
 
-export default BusinessPromotion
+export default BusinessPromotion;
