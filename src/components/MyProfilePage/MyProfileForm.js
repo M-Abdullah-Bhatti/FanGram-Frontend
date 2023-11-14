17e1c985dd2fc7efc1cf2c5954b745528beef2dd
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-11-14 19:26:46 +0500

    profile edit form updated

--- src/components/MyProfilePage/MyProfileForm.js
+++ src/components/MyProfilePage/MyProfileForm.js
@@ -14,7 +14,10 @@ const MyProfileForm = () => {
     dob: "",
     phone: "",
     email: "",
+    image: ""
   });
+  const [avatarPreview, setAvatarPreview] = useState("/images/Profile.png");
+  const [avatar, setAvatar] = useState();
 
   const handleInputChange = (e) => {
     setUserData({
@@ -30,18 +33,75 @@ const MyProfileForm = () => {
     });
   };
 
+  const registerDataChange = (e) => {
+    if (e.target.name === "avatar") {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setAvatarPreview(reader.result);
+          setAvatar(reader.result);
+        }
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setUserData({
+      ...userData,
+      image: avatar,
+    });
     updateUserMutation.mutate(userData);
   };
 
   return (
     <div className="mt-[30px] lg:m-[50px] grid place-items-center">
       <form onSubmit={handleSubmit} className="w-[100%] lg:w-[80%]  bg-[#292929] rounded-2xl">
-        <div className="mt-[100px] lg:mt-[184px] grid place-items-center">
-          <div className="w-fit bg-[#D42978] p-2 rounded-full">
-            {myProfilePageIcons.icon}
-          </div>
+        <div className="mt-[100px] lg:mt-[50px] grid place-items-center">
+          <div className="w-[10vmax] h-[10vmax]">
+              <img
+                src={avatarPreview}
+                alt=""
+                className="w-full h-full rounded-full border border-[#f1f1f1]"
+              />
+            </div>
+            <div
+              id="registerImage"
+              className="relative overflow-hidden cursor-pointer mt-4 p-2 cursor-pointer"
+            >
+              <input
+                type="file"
+                name="avatar"
+                id="fileInput"
+                accept="image/*"
+                onChange={registerDataChange}
+                className="absolute top-0 left-0 opacity-0 h-full w-full  "
+              />
+              <label for="fileInput">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="bg-[#D42978] text-white rounded-full cursor-pointer"
+                >
+                  <g clip-path="url(#clip0_92_33207)">
+                    <path
+                      d="M10.5804 9.07985C10.5804 10.5027 9.4229 11.6603 8 11.6603C6.5771 11.6603 5.42003 10.5027 5.42003 9.07985C5.42003 7.65695 6.5771 6.49943 8 6.49943C9.4229 6.49943 10.5804 7.65739 10.5804 9.07985ZM16 5.41578V12.7448C16 13.214 15.8136 13.664 15.4818 13.9958C15.15 14.3276 14.7 14.514 14.2308 14.514H1.76923C1.3 14.514 0.849989 14.3276 0.518194 13.9958C0.1864 13.664 0 13.214 0 12.7448V5.41578C0 4.94655 0.1864 4.49654 0.518194 4.16475C0.849989 3.83295 1.3 3.64655 1.76923 3.64655H3.94538V3.0344C3.94538 2.62383 4.10848 2.23007 4.3988 1.93975C4.68912 1.64943 5.08287 1.48633 5.49345 1.48633H10.5066C10.9171 1.48633 11.3109 1.64943 11.6012 1.93975C11.8915 2.23007 12.0546 2.62383 12.0546 3.0344V3.64611H14.2308C15.2078 3.64655 16 4.43873 16 5.41578ZM11.9073 9.07985C11.9073 6.92537 10.1545 5.17251 8 5.17251C5.84597 5.17251 4.09311 6.92537 4.09311 9.07985C4.09311 11.2343 5.84597 12.9872 8 12.9872C10.1545 12.9872 11.9073 11.2343 11.9073 9.07985Z"
+                      fill="white"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_92_33207">
+                      <rect width="16" height="16" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </label>
+            </div>
         </div>
         <div className="py-8 px-10">
           <label for="name">Name</label>
