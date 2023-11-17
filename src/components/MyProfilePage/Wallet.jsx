ccf959e0efa81264e73bcf5260437882b8a01f2e
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-11-18 01:59:40 +0500

    wallet page complete except coupen

--- src/components/MyProfilePage/Wallet.jsx
+++ src/components/MyProfilePage/Wallet.jsx
@@ -8,7 +8,7 @@ function Wallet() {
 
   const { data: myBookingData, isLoading: bookingLoading } =
     useAllMyOrders(userId);
-  console.log("myBookingData: ", myBookingData);
+  console.log("myBookingData: ", myBookingData?.data?.data);
 
   const { data: profileData, isLoading: profileLoading } =
     useGetUserInfo(userId);
@@ -20,6 +20,15 @@ function Wallet() {
     console.log("uuuuuu: ", userInfo?.userId);
   }, []);
 
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // January is 0
+    const year = date.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  }
+
   return (
     <div className="md:mx-28 my-8 space-y-4">
       <div className="flex flex-col md:flex-row justify-between bg-[#202020] rounded-lg px-6 py-3 font-bold">
@@ -30,7 +39,9 @@ function Wallet() {
           <p className="text-xl">
             <span className="text-[#D42978]">tcash</span> Earned
           </p>
-          <p className="text-2xl text-[#D42978]">0</p>
+          <p className="text-2xl text-[#D42978]">
+            {profileData?.tcashEarned || 0}
+          </p>
         </div>
         <div className="flex justify-center flex-col w-full md:w-1/2 md:ml-6 space-y-2 py-3">
           <p className="text-xl">Coupons</p>
@@ -55,31 +66,36 @@ function Wallet() {
               </tr>
             </thead>
             <tbody>
-              {transactionsData.map((item, index) => (
-                <tr key={index} className="text-xs md:text-base">
-                  <td className="flex pr-2 items-center gap-2 py-2">
-                    <img
-                      src="/images/wallet__icon.svg"
-                      alt="icon"
-                      className="w-[16px] md:w-[30px]"
-                    />
-                    {item.activity}
-                  </td>
-                  <td className="px-2 py-2">{item.dateTime}</td>
-                  <td className="px-2 py-2">{item.earned}</td>
-                  <td
-                    className="px-2 py-2"
-                    style={{
-                      color:
-                        item.paymentStatus === "Success"
-                          ? "#08C949"
-                          : "#FF5353",
-                    }}
-                  >
-                    {item.paymentStatus}
+              {!bookingLoading && myBookingData?.data?.data?.length > 0 ? (
+                myBookingData.data.data.map((item, index) => (
+                  <tr key={index} className="text-xs md:text-base">
+                    <td className="flex pr-2 items-center gap-2 py-2">
+                      <img
+                        src="/images/wallet__icon.svg"
+                        alt="icon"
+                        className="w-[16px] md:w-[30px]"
+                      />
+                      {item?.celebrityID?.name}
+                    </td>
+                    <td className="px-2 py-2">{formatDate(item?.createdAt)}</td>
+                    <td className="px-2 py-2">{item.price}</td>
+                    <td
+                      className="px-2 py-2"
+                      style={{
+                        color: "#08C949",
+                      }}
+                    >
+                      {"success"}
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="text-center py-4">
+                    No bookings found
                   </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
