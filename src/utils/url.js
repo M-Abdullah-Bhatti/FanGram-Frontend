e087759b3f1d1976f4aab8ca18c6bf26038bd199
Muhammad Abdullah <88588371+AFourAbdullah@users.noreply.github.com>
2024-04-12 23:09:01 +0500

    Update url.js

--- src/utils/url.js
+++ src/utils/url.js
@@ -3,7 +3,8 @@ const environment = "production";
 
 let apiUrl;
 if (environment === "production") {
-  apiUrl = "https://calm-gray-haddock-sock.cyclic.app";
+  // apiUrl = "https://calm-gray-haddock-sock.cyclic.app";
+  apiUrl = "https://fangram-backend.vercel.app";
 } else {
   apiUrl = "http://localhost:5000";
 }
